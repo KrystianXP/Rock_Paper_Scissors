0341e3748d9d531de5f824ef303f883d4466ae9b
KrystianXP <158336297+KrystianXP@users.noreply.github.com>
2024-03-28 21:08:53 +0100

    Game is finished Last steps was writing a function that call theGame function 5 times then count scores of player and computer and return the score at the end .

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 /*
     pseudocode
     Game will be against computer so:
-    Comupter Choice
+Comupter Choice
     1.Computer randomly choosing "Rock" or "Paper" or "Scissors"
     2.Display anwser
-    Player Choice
+Player Choice
     1.Player choice his pick "Rock" or "Paper" or "Scissors"
     2.Comparing player choice with comupter choice
     3.Picking a winner
     4.Displaying winner name
+Last chapter
+    1.Playing rock paper scissors five times
+    2.Keep score after each game
+    3.Report a winner at the end of loop
+
 */
 /* getComupterChoice - Randomly choosing Rock Paper or Scissors and returning anwser*/
 function getComupterChoice(){
@@ -16,28 +21,59 @@ function getComupterChoice(){
     let anwser = choices[Math.round(Math.random() * 2) ] ;
     return anwser;
 }
-let playerSelection = prompt("Choose wisely: Rock, Paper or Scissors").toLowerCase();
+
 let computerSelection = getComupterChoice();
+let playerScore = 0;
+let computerScore = 0;
 /*theGame - Checking is the playerSelection correct then looking for a right comaperation then returning anwser */ 
 function theGame(){
-    if(playerSelection == "rock" | "paper" | "scissors" ){
+    let playerSelection = prompt("Choose wisely: Rock, Paper or Scissors").toLowerCase();
+
+    if(playerSelection == "rock"||"paper"||"scissors"){
         if
         (playerSelection == "rock" && computerSelection == "scissors"){
+            playerScore = playerScore + 1
         return("Player Won!! " + playerSelection + " beats " + computerSelection)  
     }else if
             (playerSelection == "paper" && computerSelection == "rock"){
+                playerScore = playerScore + 1
             return("Player Won!! " + playerSelection + "beats " + computerSelection)   
     }else if
         (playerSelection == "scissors" && computerSelection == "paper"){
+            playerScore = playerScore + 1
             return("Player Won!! " + playerSelection + "beats " + computerSelection)       
     }else if
         (playerSelection == computerSelection){
+            
             return(playerSelection + " = " + computerSelection + " It is a draw")
-    }else{
-        return("Computer Won "+ computerSelection+" beats "+playerSelection)  
-    }
+    }else if
+        (computerSelection == "paper" && playerSelection == "rock"){
+            computerScore = computerScore + 1
+        return("Computer Won " + computerSelection + "beats " + playerSelection) 
+    }else if
+    (computerSelection == "rock" && playerSelection == "scissors"){
+        computerScore = computerScore + 1
+    return("Computer Won " + computerSelection + "beats " + playerSelection) 
+}else if
+(computerSelection == "scissors" && playerSelection == "paper"){
+    computerScore = computerScore + 1
+return("Computer Won " + computerSelection + "beats " + playerSelection) 
+}
 }else{
+    
     return("Wrong input choose one of this: Rock, Paper or Scissors")
 }
 }
-console.log(theGame())
+function playGame(){
+    console.log(theGame());
+    console.log(theGame());
+    console.log(theGame());
+    console.log(theGame());
+    console.log(theGame());
+    if(playerScore > computerScore){
+        console.log("Player won "+ playerScore + " to " + computerScore)
+    }else{
+        console.log("Compuer won " + computerScore+ " to " + playerScore)
+    }
+};
+console.log(playGame())
