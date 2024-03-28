17c91ec3fd26c5f231f9f255cb96729c042979de
KrystianXP <158336297+KrystianXP@users.noreply.github.com>
2024-03-28 20:26:35 +0100

    One match of the game function is playing one game of rock paper scissors comparing who won and returning anwser

--- script.js
+++ script.js
@@ -9,12 +9,35 @@
     2.Comparing player choice with comupter choice
     3.Picking a winner
     4.Displaying winner name
-
 */
-
+/* getComupterChoice - Randomly choosing Rock Paper or Scissors and returning anwser*/
 function getComupterChoice(){
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
     let anwser = choices[Math.round(Math.random() * 2) ] ;
     return anwser;
 }
-console.log(getComupterChoice())
+let playerSelection = prompt("Choose wisely: Rock, Paper or Scissors").toLowerCase();
+let computerSelection = getComupterChoice();
+/*theGame - Checking is the playerSelection correct then looking for a right comaperation then returning anwser */ 
+function theGame(){
+    if(playerSelection == "rock" | "paper" | "scissors" ){
+        if
+        (playerSelection == "rock" && computerSelection == "scissors"){
+        return("Player Won!! " + playerSelection + " beats " + computerSelection)  
+    }else if
+            (playerSelection == "paper" && computerSelection == "rock"){
+            return("Player Won!! " + playerSelection + "beats " + computerSelection)   
+    }else if
+        (playerSelection == "scissors" && computerSelection == "paper"){
+            return("Player Won!! " + playerSelection + "beats " + computerSelection)       
+    }else if
+        (playerSelection == computerSelection){
+            return(playerSelection + " = " + computerSelection + " It is a draw")
+    }else{
+        return("Computer Won "+ computerSelection+" beats "+playerSelection)  
+    }
+}else{
+    return("Wrong input choose one of this: Rock, Paper or Scissors")
+}
+}
+console.log(theGame())
